bdf23b0bec4ddd363e7ca0fa6e1788873ab31da6
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2017-04-24 10:51:05 +0300

    Transparent code editor

--- app/src/mainwindow.cpp
+++ app/src/mainwindow.cpp
@@ -9,16 +9,13 @@ static const char* fshader =
 	"#ifdef GL_ES\n"
 	"precision highp int;\n"
 	"precision highp float;\n"
-	"#endif\n"
+	"#endif\n\n"
 
-	"varying highp vec2 pixelPosition;\n"
+	"varying highp vec2 pixelPosition;\n\n"
 
 	"void main()\n"
 	"{\n"
-		"if (pixelPosition.x > 100.0)\n"
-			"\tgl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n"
-		"else\n"
-			"\tgl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n"
+		"\tgl_FragColor = pixelPosition.x > 100.0 ? vec4(1.0, 0.95, 0.0, 1.0) : vec4(0.0, 0.85, 1.0, 1.0);\n"
 	"}\n";
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -28,15 +25,15 @@ MainWindow::MainWindow(QWidget *parent) :
 	ui->setupUi(this);
 
 	_renderWidget = new ShaderRenderWidget(ui->shaderWidgetHost);
-	_shaderEditorWidget = new QTextEdit(ui->shaderWidgetHost);
 
+	_shaderEditorWidget = new QTextEdit(ui->shaderWidgetHost);
 	_shaderEditorWidget->setLineWrapMode(QTextEdit::NoWrap);
 	auto editorPalette = _shaderEditorWidget->palette();
-	editorPalette.setColor(QPalette::Background, Qt::transparent);
+	editorPalette.setColor(QPalette::Active, QPalette::Base, Qt::transparent);
+	editorPalette.setColor(QPalette::Inactive, QPalette::Base, Qt::transparent);
 	_shaderEditorWidget->setPalette(editorPalette);
-
 	_shaderEditorWidget->setText(fshader);
-	_shaderEditorWidget->setTabStopWidth(3 * _shaderEditorWidget->fontMetrics().width(' '));
+	_shaderEditorWidget->setTabStopWidth(4 * _shaderEditorWidget->fontMetrics().width(' '));
 	connect(_shaderEditorWidget, &QTextEdit::textChanged, this, &MainWindow::updateFragmentShader);
 }
 
