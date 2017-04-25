9ad5a41472201ed210ed60e96db77e0885196765
VioletGiraffe <noreply@bitbucket.org>
2017-04-25 08:29:04 +0300

    Cosmetic edits in CodeEditor

--- app/src/codeeditor.cpp
+++ app/src/codeeditor.cpp
@@ -16,7 +16,6 @@ CodeEditor::CodeEditor(QWidget *parent) : QPlainTextEdit(parent)
 	highlightCurrentLine();
 }
 
-
 int CodeEditor::lineNumberAreaWidth()
 {
 	int digits = 1;
@@ -31,12 +30,10 @@ int CodeEditor::lineNumberAreaWidth()
 	return space;
 }
 
-
 void CodeEditor::updateLineNumberAreaWidth(int /* newBlockCount */)
 {
 	setViewportMargins(lineNumberAreaWidth(), 0, 0, 0);
 }
-
 
 void CodeEditor::updateLineNumberArea(const QRect &rect, int dy)
 {
@@ -49,7 +46,6 @@ void CodeEditor::updateLineNumberArea(const QRect &rect, int dy)
 		updateLineNumberAreaWidth(0);
 }
 
-
 void CodeEditor::resizeEvent(QResizeEvent *e)
 {
 	QPlainTextEdit::resizeEvent(e);
@@ -58,26 +54,21 @@ void CodeEditor::resizeEvent(QResizeEvent *e)
 	lineNumberArea->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
 }
 
-
 void CodeEditor::highlightCurrentLine()
 {
-	QList<QTextEdit::ExtraSelection> extraSelections;
+	if (isReadOnly())
+		return;
 
-	if (!isReadOnly()) {
-		QTextEdit::ExtraSelection selection;
+	const QColor lineColor = QColor(64, 64, 64, 64);
 
-		QColor lineColor = QColor(Qt::yellow).lighter(160);
+	QTextEdit::ExtraSelection selection;
+	selection.format.setBackground(lineColor);
+	selection.format.setProperty(QTextFormat::FullWidthSelection, true);
+	selection.cursor = textCursor();
+	selection.cursor.clearSelection();
 
-		selection.format.setBackground(lineColor);
-		selection.format.setProperty(QTextFormat::FullWidthSelection, true);
-		selection.cursor = textCursor();
-		selection.cursor.clearSelection();
-		extraSelections.append(selection);
-	}
-
-	setExtraSelections(extraSelections);
+	setExtraSelections(QList<QTextEdit::ExtraSelection>{selection});
 }
-
 
 void CodeEditor::lineNumberAreaPaintEvent(QPaintEvent *event)
 {
@@ -87,20 +78,20 @@ void CodeEditor::lineNumberAreaPaintEvent(QPaintEvent *event)
 
 	QTextBlock block = firstVisibleBlock();
 	int blockNumber = block.blockNumber();
-	int top = (int) blockBoundingGeometry(block).translated(contentOffset()).top();
-	int bottom = top + (int) blockBoundingRect(block).height();
+	int top = (int)blockBoundingGeometry(block).translated(contentOffset()).top();
+	int bottom = top + (int)blockBoundingRect(block).height();
 
 	while (block.isValid() && top <= event->rect().bottom()) {
 		if (block.isVisible() && bottom >= event->rect().top()) {
 			QString number = QString::number(blockNumber + 1);
 			painter.setPen(Qt::black);
 			painter.drawText(0, top, lineNumberArea->width(), fontMetrics().height(),
-							 Qt::AlignRight, number);
+				Qt::AlignRight, number);
 		}
 
 		block = block.next();
 		top = bottom;
-		bottom = top + (int) blockBoundingRect(block).height();
+		bottom = top + (int)blockBoundingRect(block).height();
 		++blockNumber;
 	}
 }
